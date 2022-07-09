0ecd41143e986a60b7ce116c06c94c5f4f9ee891
Kelvin F. Baez Rogriguez <kelvin12378945610@gmail.com>
2022-07-09 00:25:25 -1200

    [add] changed project - no webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import "./styles.css";
-import "./css/header.css";
-import "./css/main-content.css";
-import "./css/about-us.css";
-import "./css/contact.css";
-import "./css/footer.css";
+console.log("hello");
