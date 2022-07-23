b93478c951e5b6100080f9ace10dd5a39fd7d508
Kelvin F. Baez Rogriguez <kelvin12378945610@gmail.com>
2022-07-23 18:23:28 -0400

    [add] login & contact form validate added

---
+++ src/js/form-validate.js
@@ -0,0 +1,116 @@
+const form = document.querySelector(".contact__from");
+const fieldValidateName = document.querySelector("#name__validate");
+const fieldValidateMessage = document.querySelector("#message__validate");
+
+const formLogin = document.querySelector(".login__from");
+const fieldValidate = document.querySelector(".field__validate");
+
+const loadingContainer = document.querySelector(".loading__container");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const data = new FormData(form);
+
+  const name = data.get("name");
+  const message = data.get("message");
+
+  //* name alert
+  if (name.trim().length < 3) {
+    fieldValidateName.classList.add("active");
+  } else {
+    fieldValidateName.classList.remove("active");
+  }
+
+  //* message alert
+  if (message.trim().length < 20) {
+    fieldValidateMessage.classList.add("active");
+  } else {
+    fieldValidateMessage.classList.remove("active");
+  }
+
+  if (name.trim().length >= 3 && message.trim().length >= 20) {
+    console.log("enviando mensaje");
+    console.log({ name, message });
+    return;
+  }
+
+  fieldValidateMessage.textContent = "Mensaje mayor a 20 caracteres";
+  fieldValidateName.textContent = "Nombre mayor a 2 caracteres";
+});
+
+// Login validate
+
+const loading = () => {
+  setTimeout(() => {
+    loadingContainer.classList.remove("active");
+  }, 2000);
+};
+
+const loadPageAdmin = () => {
+  setTimeout(() => {
+    // load admin page
+    window.location = "../../administration-product.html";
+  }, 2200);
+};
+
+const fetchData = async (email, pass) => {
+  try {
+    const res = await fetch(
+      "https://orac-e-commerce-project.herokuapp.com/perfil"
+    );
+
+    const data = await res.json();
+
+    data.forEach(({ email: userEmail, password: userPass }) => {
+      if (userEmail === email && userPass === pass) {
+        fieldValidate.textContent = "";
+
+        //* active loading
+        loadingContainer.classList.add("active");
+
+        loadPageAdmin();
+        return;
+      } else {
+        throw "Contraseña Incorrecta/Correo Incorrecto";
+      }
+    });
+  } catch (error) {
+    throw new Error(error);
+  } finally {
+    // loading function
+    loading();
+  }
+};
+
+formLogin?.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  //* regex
+  const reg = new RegExp(
+    "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
+    "g"
+  );
+
+  const data = new FormData(formLogin);
+
+  const email = data.get("email");
+  const password = data.get("password");
+
+  if (reg.test(email) && password.trim().length >= 5) {
+    fetchData(email, password)
+      .catch((err) => {
+        console.log(err);
+        fieldValidate.textContent = err;
+      })
+      .then((msg) => {
+        console.log(msg);
+      });
+  }
+
+  //* Alerts
+  fieldValidate.classList.add("active");
+  fieldValidate.classList.remove("activeValid");
+
+  fieldValidate.textContent =
+    "Contraseña mayor a 4 caracteres/Correo no valido";
+});
---
+++ src/js/index.js
@@ -0,0 +1,1 @@
+import "./form-validate.js";
