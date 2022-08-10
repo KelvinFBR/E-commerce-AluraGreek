64656c233d93f6600b83bd6250349ade00f1456f
Kelvin F. Baez Rogriguez <kelvin12378945610@gmail.com>
2022-08-09 22:01:55 -0400

    [fix] url login & admin page v1

--- src/js/index.js
+++ src/js/index.js
@@ -16,7 +16,7 @@ const pathName = window.location.pathname;
 authenticationInit();
 
 if (
-  /E-commerce-AluraGreek\//g.test(pathName) ||
+  /\//g.test(pathName) ||
   /E-commerce-AluraGreek\/index.html/g.test(pathName) ||
   /index.html/g.test(pathName)
 ) {
